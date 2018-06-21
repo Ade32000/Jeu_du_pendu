02b0afe8c68d3441237ba7487d04fd4aace2bfb0
Adeline <adeline.contact@gmail.com>
2018-06-21 19:17:41 +0200

    code complet

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var canvas  = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
-
-var mot = ['simplon','code','programmation','javascript'];
+var compteur = 0;
+var nbVies = 11;
+var mot = ['simplon','code','programmation','javascript','ordinateur', 'halloween', 'partage', 'loop'];
 var operation = Math.floor(Math.random() * mot.length);
 mot = mot[operation];
 var taille = mot.length;
@@ -9,30 +10,169 @@ var mot2 = mot;
 mot = mot.split("");
 console.log(mot);
 var tableau_reponse = [];
-
-var mot_util = prompt('Veuillez saisir une lettre');
-console.log(mot_util);
-
-for (var i = 0; i < mot.length ; i++)
-{
-	tableau_reponse[i] = '_ ';
-	$('#motcache').html(tableau_reponse);
-}
-console.log(tableau_reponse);
-
-
-for (var j = 0; j < mot.length ; j++)
-{
-	var temp = jQuery.inArray(mot_util, mot);
-	console.log(temp);
-	for (j = 6; j < mot.length-5 ; j++)
+var mot_util = "";
+$('#lettre').focus(); //focus dans l'input
+$('#lettre').val("");
+var compare = '';
+// Remplace les lettres du mot caché tiré au hasard par des underscores et les stocke dans un nouveau tableau
+	for (var i = 0; i < mot.length ; i++)
 	{
-		var tmp = jQuery.inArray(mot_util, mot);
-		console.log(tmp);
+		tableau_reponse[i] = '_ ';
+		$('#motcache').html(tableau_reponse).css('line-height' , '70px').css( 'font-size' , '68px');
 	}
-}
+
+	console.log(tableau_reponse);
 
 
+//fonction du jeu pour 1 manche
+	$('#valider').click(function jouer()
+	{
+
+		mot_util = $('#lettre').val();
+		mot_util = mot_util.toLowerCase();
+		$('#lettre').fadeOut().delay().fadeIn('slow'); //Fait disparaître l'input avec une animation
+		console.log(mot_util);
+
+			if (mot_util.length === 0)
+			{
+				alert('Merci de saisir une lettre');
+			}
+			else if (mot_util.length > 1)
+			{
+				alert('Vous avez saisi plusieurs lettres, merci de n\'en saisir qu\'une!');
+			}
+			else
+			{
+
+// trouves le nombre d'occurences de la lettre tapée, et renvoie son/ses index
+				var index = [];
+				var élément = mot_util;
+				var idx = mot.indexOf(élément);
+					while (idx != -1) 
+					{
+		  				index.push(idx);
+		  				idx = mot.indexOf(élément, idx + 1);
+					}
+		
+				console.log(index);
+
+// Si une lettre est correcte
+					if (index.length != 0)
+					{
+// Récupère les index trouvés et les stocke dans des variables individuellement
+							for (var i = 0; i< index.length; i++)
+							{
+								var a = index[0];
+								var b = index[1];
+								var c = index[2];
+								console.log(a);
+							}
+// Affiche les lettres trouvées dans le tableau contenant les 'underscores' aux index appropriés
+							for (var j =0; j<tableau_reponse.length; j++)
+							{
+								tableau_reponse[a] = mot_util;
+								tableau_reponse[b] = mot_util;
+								tableau_reponse[c] = mot_util;
+								$('#motcache').html(tableau_reponse);
+							}
+							
+							compare = tableau_reponse.join('');
+							console.log(compare);
+							if (compare === mot2)
+							{
+								if (compteur === 0)
+								{	
+									alert('Félicitations !!! Vous avez gagné sans avoir commis d\'erreur!');
+								}
+								else
+								{
+									alert('Félicitations !!! Vous avez gagné au bout de : ' + compteur + ' erreurs');
+								}
+
+									var rejoue = confirm('Voulez-vous rejouer?');
+										if (rejoue == true)
+										{
+											document.location.reload(true);
+											// jouer();
+										}
+										else
+										{
+											alert('Vous avez choisi de quitter ce magnifique programme. Tant pis pour vous !');
+										}
+							}
+
+					}
+					else //lettre non présente dans le mot, implémente le compteur et affiche les éléments du canvas
+					{
+							compteur ++; 
+							$('#result').html("lettre non présente ! Vies restantes : " + (nbVies - compteur));
+
+
+								if (compteur === 1) 
+								{
+									base();
+								}
+								if (compteur === 2) 
+								{
+									poteau();
+								}
+								if (compteur === 3) 
+								{
+									poutrehaut();
+								}
+								if (compteur === 4) 
+								{
+									renfort();
+								}
+								if (compteur === 5) 
+								{
+									corde();
+								}
+								if (compteur === 6) 
+								{
+									tete();
+								}
+								if (compteur === 7) 
+								{
+									corps();
+								}
+								if (compteur === 8) 
+								{
+									brasg();
+								}
+								if (compteur === 9) 
+								{
+									brasd();
+								}
+								if (compteur === 10) 
+								{
+									jambeg();
+								}
+								if (compteur === 11) 
+								{
+									jambed();
+									alert('Perdu ! Le mot était : ' + mot2);
+									var rejouer = confirm('Voulez-vous rejouer?');
+										if (rejouer == true)
+										{
+											document.location.reload(true);
+											// jouer();
+										}
+										else
+										{
+											document.location.reload(true);
+											alert('Vous avez choisi de quitter ce magnifique programme. Tant pis pour vous !');
+										}
+								}
+						
+					}
+
+			}
+			$('#lettre').val(""); //Fait disparaître la valeur de l'input
+			$('#lettre').focus(); //Fait revenir le focus sur l'input
+	});
+
+// Dessins du canvas 
 
 // Base
 function base ()
@@ -43,9 +183,10 @@ context.lineCap = 'round';
 context.moveTo(60, 400);
 context.lineTo(400, 400);
 context.stroke();
-}
+};
 
 // Poteau
+
 function poteau ()
 {
 context.beginPath();
@@ -54,7 +195,9 @@ context.lineCap = 'round';
 context.moveTo(90, 70);
 context.lineTo(90, 400);
 context.stroke();
-}
+};
+
+
 
 // Poutre du haut
 function poutrehaut ()
@@ -151,7 +294,7 @@ context.moveTo(240, 250);
 context.lineTo(290, 285);
 context.stroke();
 }
-
+/*
 base();
 poteau();
 poutrehaut();
@@ -163,4 +306,4 @@ brasg();
 brasd();
 jambeg();
 jambed();
-
+*/
